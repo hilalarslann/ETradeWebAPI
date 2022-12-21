5138f58ee88fdf7e11765514103f4c2fb18889aa
Emre <emremusluk9099@gmail.com>
2022-12-21 17:45:27 +0300

    BasketMaster and Vat Controller Added

---
+++ ETradeProject/Controllers/BasketMasterController.cs
@@ -0,0 +1,59 @@
+﻿using ETrade.DTO;
+using ETrade.Entities.Concrete;
+using ETrade.UI.HttpResponse;
+using ETrade.UoW;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ETrade.UI.Controllers
+{
+
+    [Route("/[controller]/[action]")]
+    [ApiController]
+    public class BasketMasterController : ControllerBase
+    {
+        IUoW _uow;
+        Response _response;
+        BasketMaster _basketmaster;
+        public BasketMasterController(IUoW uow, Response response, BasketMaster basketMaster)
+        {
+            _uow = uow;
+            _response = response;
+            _basketmaster = basketMaster;
+        }
+        [HttpPost]
+        public Response Create()
+        {
+            var usr = JsonConvert.DeserializeObject<UserDTO>(HttpContext.Session.GetString("User"));
+            var selectedBasket = _uow._BasketMasterRep.Set().FirstOrDefault(x => x.Completed == false && x.EntityId == usr.Id);
+            try
+            {
+                if (selectedBasket != null)
+                {
+                    _response.Error = true;
+                    _response.Msg = "Zaten mevcut Sepetiniz Var";
+                    return _response;
+                }
+                //burada geri döndürürken id=selectedbasket.Id ye eşitlenmiş ama bi burada yapmadık.
+                else
+                {
+                    _basketmaster.OrderDate = DateTime.Now;
+                    _basketmaster.EntityId = usr.Id;
+                    _uow._BasketMasterRep.Add(_basketmaster);
+                    _uow.Commit();
+                    _response.Error = false;
+                    _response.Msg = "Yeni Sepetiniz Oluşturuldu.";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                _response.Error = true;
+                _response.Msg = ex.Message;
+            }
+            return _response;
+        }
+
+    }
+}
---
+++ ETradeProject/Controllers/VatController.cs
@@ -0,0 +1,63 @@
+﻿using ETrade.Entities.Concrete;
+using ETrade.UI.HttpResponse;
+using ETrade.UoW;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETrade.UI.Controllers
+{
+
+    [Route("/[controller]/[action]")]
+    [ApiController]
+    public class VatController : ControllerBase
+    {
+        IUoW _uow;
+        Response _response;
+        public VatController(IUoW uow, Response response)
+        {
+            _uow = uow;
+            _response = response;
+        }
+        [HttpGet]
+        public List<Vat> ListVat()
+        {
+            return _uow._VatRep.List();
+        }
+        [HttpPost]
+        public Response AddVat(Vat vat)
+        {
+            try
+            {
+                _uow._VatRep.Add(vat);
+                _response.Error = false;
+                _response.Msg = "Başarı ile eklendi";
+                _uow.Commit();
+            }
+            catch (Exception ex)
+            {
+                _response.Error = false;
+                _response.Msg = ex.Message;
+                throw;
+            }
+            return _response;
+        }
+        [HttpDelete("{id}")]
+        public Response DeleteVat(int id)
+        {
+            try
+            {
+                _uow._VatRep.Delete(id);
+                _response.Error = false;
+                _response.Msg = "Başarı İle Silindi.";
+                _uow.Commit();
+
+            }
+            catch (Exception ex)
+            {
+                _response.Error = false;
+                _response.Msg = ex.Message;
+            }
+            return _response;
+        }
+    }
+}
